c8d16124e7dfa6901e156aba1b781384cfa92c09
Boelensman1 <me@wigger.email>
2025-03-08 12:17:18 +0100

    Show message when required permissons are not granted
    Signed-off-by: Boelensman1 <me@wigger.email>

--- app/app/src/main/java/com/wboelens/polarrecorder/managers/PermissionManager.kt
+++ app/app/src/main/java/com/wboelens/polarrecorder/managers/PermissionManager.kt
@@ -45,11 +45,35 @@ class PermissionManager(private val activity: Activity) {
             if (allGranted) {
               pendingPermissionCallback?.invoke()
             } else {
-              // TODO: show message explaining why we need these permissions
+              showPermissionRationaleMessage()
             }
           }
 
   private var pendingPermissionCallback: (() -> Unit)? = null
+
+  private fun showPermissionRationaleMessage() {
+    val message =
+        when {
+          Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> {
+            "This app requires Bluetooth and notification permissions to connect to your Polar device and run in the background."
+          }
+          Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            "This app requires Bluetooth permissions to connect to your Polar device and record data."
+          }
+          else -> {
+            "This app requires location and Bluetooth permissions to discover and connect to your Polar device."
+          }
+        }
+
+    android.app.AlertDialog.Builder(activity)
+        .setTitle("Permissions Required")
+        .setMessage(message)
+        .setPositiveButton("Try Again") { _, _ ->
+          checkAndRequestPermissions(pendingPermissionCallback ?: {})
+        }
+        .setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss() }
+        .show()
+  }
 
   fun checkAndRequestPermissions(onPermissionsGranted: () -> Unit) {
     val missingPermissions =
