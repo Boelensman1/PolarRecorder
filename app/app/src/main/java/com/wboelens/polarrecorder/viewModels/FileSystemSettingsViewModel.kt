c39803efd6ab316abd48d6ad83ce69215855e128
Boelensman1 <me@wigger.email>
2025-07-03 09:04:34 +0200

    Set initial file selector directory to downloads folder

--- app/app/src/main/java/com/wboelens/polarrecorder/viewModels/FileSystemSettingsViewModel.kt
+++ app/app/src/main/java/com/wboelens/polarrecorder/viewModels/FileSystemSettingsViewModel.kt
@@ -3,6 +3,8 @@ package com.wboelens.polarrecorder.viewModels
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Environment
+import android.provider.DocumentsContract
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -18,6 +20,12 @@ class FileSystemSettingsViewModel : ViewModel() {
       addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
       addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
       addFlags(Intent.FLAG_GRANT_PREFIX_URI_PERMISSION)
+
+      // Set initial directory to Downloads folder
+      val downloadsDir =
+          Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+      val initialUri = Uri.fromFile(downloadsDir)
+      putExtra(DocumentsContract.EXTRA_INITIAL_URI, initialUri)
     }
   }
 
