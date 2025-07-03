d7d41a7eaa2517be5a77056d84bf83dc88973838
Boelensman1 <me@wigger.email>
2025-07-03 09:33:33 +0200

    Bump version

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
     applicationId = "com.wboelens.polarrecorder"
     minSdk = 24
     targetSdk = 35
-    versionCode = 14
-    versionName = "1.2.1"
+    versionCode = 15
+    versionName = "1.2.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
