bcacb179e3a87a86f23c38dd8f6ebce8d9c3facb
Boelensman1 <me@wigger.email>
2025-07-03 09:20:25 +0200

    Bump target SDK

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
   defaultConfig {
     applicationId = "com.wboelens.polarrecorder"
     minSdk = 24
-    targetSdk = 34
+    targetSdk = 35
     versionCode = 14
     versionName = "1.2.1"
 
