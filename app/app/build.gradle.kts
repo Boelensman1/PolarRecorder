521ca50b82a511673148f942ed98cd924b132037
Boelensman1 <me@wigger.email>
2025-07-11 19:06:14 +0200

    Bump version to 2.0.0
    Signed-off-by: Boelensman1 <me@wigger.email>

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
     applicationId = "com.wboelens.polarrecorder"
     minSdk = 24
     targetSdk = 35
-    versionCode = 15
-    versionName = "1.2.2"
+    versionCode = 16
+    versionName = "2.0.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
